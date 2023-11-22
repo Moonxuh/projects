cad27dc2d5e69e2d4a36eb0203d2ce5fd9afc1d5
Lara <laralansone64@gmail.com>
2023-11-22 18:10:58 -0300

    Finalização da funcionalidade da lampada.

--- Light Bulb/lampada.js
+++ Light Bulb/lampada.js
@@ -1,9 +1,20 @@
 var lamp = window.document.getElementById('lampada')
 
+function estaQuebrada() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
 function Ligar() {
-    lamp.src = 'ligada.svg'
+    if (!estaQuebrada()) {
+    lamp.src = 'ligada.svg'}
 }
 
 function Desligar() {
-    lamp.src = 'desligada.svg'
+    if (!estaQuebrada()) {
+    lamp.src = 'desligada.svg'}
+}
+
+lamp.addEventListener ('click', Quebrar)
+function Quebrar() {
+    lamp.src = 'quebrada.svg'
 }
