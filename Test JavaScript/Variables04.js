0e6d2033a4d7a7799b204603602ed35cb3970488
Lara <laralansone64@gmail.com>
2024-03-15 14:44:56 -0300

    Var 04 finalizada.

---
+++ Test JavaScript/Variables04.js
@@ -0,0 +1,7 @@
+var selecao = 'Chile'
+
+if (selecao == 'Brasil' || selecao == 'Argentina' || selecao == 'Uruguai') {
+    console.log(`${selecao} já ganhou a Copa.`)
+} else {
+    console.log(`${selecao} nunca ganhou uma Copa do Mundo.`)
+}
