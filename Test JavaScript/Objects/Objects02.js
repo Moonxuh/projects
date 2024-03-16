39a0f134b360d944716a384de6abf75f34358936
Lara <laralansone64@gmail.com>
2024-03-16 16:34:38 -0300

    Creating a new folder and completing the creation of the 'Objects02.js' file.

---
+++ Test JavaScript/Objects/Objects02.js
@@ -0,0 +1,15 @@
+var selecao = [
+    {
+      nome: "Neimar",
+      funcao: "Atacante"
+    },
+    {
+      nome: "Renato",
+      funcao: "Meio Campo"
+    },
+    {
+      nome: "Rogério",
+      funcao: "Goleiro"
+    },
+]
+console.log(selecao[2].nome)
