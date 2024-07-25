d81612e2b4372fef0b7d318590d68235a96dc9c1
Lara <laralansone64@gmail.com>
2024-07-25 18:53:06 -0300

    Finalizando 'Conditional03'.

---
+++ Test JavaScript/Conditionals/Conditional03.js
@@ -0,0 +1,16 @@
+let fruit = "banana";
+
+switch (fruit) {
+  case "apple":
+    console.log("A fruta é maçã.");
+    break;
+  case "banana":
+    console.log("A fruta é banana.");
+    break;
+  case "orange":
+    console.log("A fruta é laranja.");
+    break;
+  default:
+    console.log("Fruta desconhecida.");
+    break;
+}
