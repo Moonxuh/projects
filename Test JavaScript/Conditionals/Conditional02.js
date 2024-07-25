27ac0ebd1e5ee6c33e6d998e32e5c039a8c4bdea
Lara <laralansone64@gmail.com>
2024-07-25 18:51:45 -0300

    Finalizando 'Conditional02'.

---
+++ Test JavaScript/Conditionals/Conditional02.js
@@ -0,0 +1,15 @@
+let age = 18;
+
+if (age >= 18) {
+  console.log("Você é um adulto.");
+} else {
+  console.log("Você é menor de idade.");
+}
+
+if (age < 13) {
+  console.log("Você é uma criança.");
+} else if (age >= 13 && age < 18) {
+  console.log("Você é um adolescente.");
+} else {
+  console.log("Você é um adulto.");
+}
